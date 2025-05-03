7daabee2859ccc3c093241d73a39b4ae487173e6
guilherme coelho <guilhermeifc7@gmail.com>
2025-05-03 11:09:30 -0300

    refactor: reorganiza a estrutura do analisador sintático e implementa a matriz de parsing

--- sintatico.js
+++ sintatico.js
@@ -1,338 +1,329 @@
-exports.tabela = {
-    PROGRAMA: {
-        program: ["program", "ident", ";", "DECLARACOES", "BLOCO", "."],
-    },
+exports.producoes = {
+    p1: ["8", "16", 31, "46", "47", 35],
+    p2: ["48", "49", "50"],
+    p3: ["21", "16", "26", "12", 31, "48"],
+    p4: ["44"],
+    p5: [2, "52", 33, "53", 31, "54"],
+    p6: ["16", "55"],
+    p7: ["44"],
+    p8: ["36", "16", "55"],
+    p9: ["52", 33, "53", 31, "54"],
+    p10: ["44"],
+    p11: ["14"],
+    p12: ["6"],
+    p13: ["5"],
+    p14: [
+    "9",
+        "16",
+        "56",
+        31,
+        "47",
+        31,
+        "50",
+    ],
+    p15: ["44"],
+    p16: [39,"52", 33, "53", "57",38],
+    p17: ["44"],
+    p18: ["36", "52", 33, "53", "57"],
+    p19: ["44"],
+    p20: ["22", "51", "18"],
+    p21: ["58", 31, "51"],
+    p22: ["44"],
+    p23: ["10", 40, "59", "60", 41],
+    p24: ["61"],
+    p25: ["44"],
+    p26: [36, "59", "60"],
+    p27: ["62", "63"],
+    p28: ["30", "62", "63"],
+    p29: [42, "62", "63"],
+    p30: ["44"],
+    p31: ["64", "65"],
+    p32: [37, "64", "65"],
+    p33: [34, "64", "65"],
+    p34: ["44"],
+    p35: [39, "61", 38],
+    p36: ["16"],
+    p37: ["12"],
+    p38: ["11"],
+    p39: ["13"],
+    p40: ["23"],
+    p41: ["15", "66", "4", "47", "67"],
+    p42: ["61", "68", "61"],
+    p43: ["26"],
+    p44: ["27"],
+    p45: ["29"],
+    p46: ["25"],
+    p47: ["28"],
+    p48: ["24"],
+    p49: ["19", "47"],
+    p50: ["44"],
+    p51: ["16", "69"],
+    p52: ["17", "16", "32", "61", "3", "61", "20", "47"],
+    p53: ["70"],
+    p54: ["32", "61"],
+    p55: [39, "71", "72", 38],
+    p56: ["44"],
+    p57: ["16"],
+    p58: ["12"],
+    p59: ["11"],
+    p60: ["23"],
+    p61: [36, "71", "72"],
+    p62: ["44"],
+    p63: ["1", "66", "20", "47"],
+    p64: ["7", 39, "16", 38],
+    p65: ["13"],
+};
 
-    DECLARACOES: {
-        const: ["CONSTANTES", "VARIAVEIS", "PROCEDIMENTOS"],
-        var: ["CONSTANTES", "VARIAVEIS", "PROCEDIMENTOS"],
-        procedure: ["CONSTANTES", "VARIAVEIS", "PROCEDIMENTOS"],
-        begin: ["CONSTANTES", "VARIAVEIS", "PROCEDIMENTOS"],
+exports.matrizParsing = {
+    45: {
+        1: "p1",
     },
-
-    CONSTANTES: {
-        const: ["const", "ident", "=", "nint", ";", "CONSTANTES"],
-        var: ["ε"],
-        procedure: ["ε"],
-        begin: ["ε"],
+    46: {
+        2: "p2",
+        21: "p2",
     },
-
-    VARIAVEIS: {
-        var: ["var", "LISTAVARIAVEIS", ":", "TIPO", ";", "LDVAR"],
+    47: {
+        22: "p20",
     },
-
-    LDVAR: {
-        ident: ["LISTAVARIAVEIS", ":", "TIPO", ";", "LDVAR"],
-        procedure: ["ε"],
-        begin: ["ε"],
+    48: {
+        2: "p4",
+        21: "p3",
     },
-
-    TIPO: {
-        integer: ["integer"],
-        real: ["real"],
-        string: ["string"],
+    49: {
+        2: "p5",
     },
-
-    LISTAVARIAVEIS: {
-        ident: ["ident", "REPIDENT"],
+    52: {
+        2: "p5",
+        16:"p6"
     },
-
-    REPIDENT: {
-        ",": [",", "ident", "REPIDENT"],
-        ":": ["ε"],
+    53: {
+        5: "p13",
+        6: "p12",
+        14: "p11",
     },
-
-    PROCEDIMENTOS: {
-        procedure: [
-            "procedure",
-            "ident",
-            "PARAMETROS",
-            ";",
-            "BLOCO",
-            ";",
-            "PROCEDIMENTOS",
-        ],
-        begin: ["ε"],
+    54: {
+        9: "p10",
+        16: "p9",
+        22: "p10",
     },
-
-    PARAMETROS: {
-        "(": ["(", "LISTAVARIAVEIS", ":", "TIPO", "REPPARAMETROS", ")"],
-        ";": ["ε"],
+    55: {
+        33: "p7",
+        36: "p8",
     },
-
-    REPPARAMETROS: {
-        ",": [",", "LISTAVARIAVEIS", ":", "TIPO", "REPPARAMETROS"],
-        ")": ["ε"],
+    56: {
+        31: "p17",
+        39: "p16",
     },
-
-    BLOCO: {
-        begin: ["begin", "COMANDOS", "end"],
+    57: {
+        36: "p18",
+        38: "p19"
     },
-
-    COMANDOS: {
-        print: ["COMANDO", ";", "COMANDOS"],
-        if: ["COMANDO", ";", "COMANDOS"],
-        ident: ["COMANDO", ";", "COMANDOS"],
-        for: ["COMANDO", ";", "COMANDOS"],
-        while: ["COMANDO", ";", "COMANDOS"],
-        read: ["COMANDO", ";", "COMANDOS"],
-        end: ["ε"],
+    50: {
+        9: "p14",
+        22: "p15",
     },
-
-    COMANDO: {
-        print: ["print", "{", "ITEMSAIDA", "REPITEM", "}"],
-        if: ["if", "EXPRELACIONAL", "then", "BLOCO", "ELSEOPC"],
-        ident: ["ident", "CHAMADAPROC"],
-        for: [
-            "for",
-            "ident",
-            ":=",
-            "EXPRESSAO",
-            "to",
-            "EXPRESSAO",
-            "do",
-            "BLOCO",
-        ],
-        while: ["while", "EXPRELACIONAL", "do", "BLOCO"],
-        read: ["read", "(", "ident", ")"],
+    51: {
+        1: "p21",
+        7: "p21",
+        10: "p21",
+        15: "p21",
+        16: "p21",
+        17: "p21",
+        18: "p22",
     },
-
-    ELSEOPC: {
-        else: ["else", "BLOCO"],
-        ";": ["ε"],
+    58: {
+        1: "p63",
+        7: "p64",
+        10: "p23",
+        15: "p41",
+        16: "p51",
+        17: "p52",
     },
-
-    CHAMADAPROC: {
-        "(": ["LISTAPARAMETROS"],
-        ":=": [":=", "EXPRESSAO"],
-        ";": ["ε"],
+    59: {
+        11: "p24",
+        12: "p24",
+        13: "p24",
+        16: "p24",
+        23: "p24",
     },
-
-    LISTAPARAMETROS: {
-        "(": ["(", "PAR", "REPPAR", ")"],
-        ";": ["ε"],
+    60: {
+        36: "p26",
+        39: "p27",
+        41: "p25",
     },
-
-    PAR: {
-        ident: ["ident"],
-        nint: ["nint"],
-        nreal: ["nreal"],
-        vstring: ["vstring"],
-        literal: ["literal"],
+    61: {
+        11: "p27",
+        12: "p27",
+        13: "p27",
+        16: "p27",
+        23: "p27",
+        39: "p31",
     },
-
-    REPPAR: {
-        ",": [",", "PAR", "REPPAR"],
-        ")": ["ε"],
+    62: {
+        11: "p31",
+        12: "p31",
+        13: "p31",
+        16: "p31",
+        23: "p31",
+        38: "p30",
     },
-
-    ITEMSAIDA: {
-        "(": ["EXPRESSAO"],
-        ident: ["EXPRESSAO"],
-        nint: ["EXPRESSAO"],
-        nreal: ["EXPRESSAO"],
-        literal: ["EXPRESSAO"],
-        vstring: ["EXPRESSAO"],
+    63: {
+        3: "p30",
+        4: "p30",
+        20: "p30",
+        "24": "p30",
+        "25": "p30",
+        "26": "p30",
+        "27": "p30",
+        "28": "p30",
+        "29": "p30",
+        "30": "p28",
+        31: "p30",
+        36: "p30",
+        39: "p35",
+        42: "p29",
+        41: "p30"
     },
-
-    REPITEM: {
-        ",": [",", "ITEMSAIDA", "REPITEM"],
-        "}": ["ε"],
+    64: {
+        11: "p38",
+        12: "p37",
+        13: "p39",
+        16: "p36",
+        23: "p40",
+        38: "p34",
+        42: "p34",
     },
-
-    EXPRESSAO: {
-        "(": ["TERMO", "EXPR"],
-        ident: ["TERMO", "EXPR"],
-        nint: ["TERMO", "EXPR"],
-        nreal: ["TERMO", "EXPR"],
-        literal: ["TERMO", "EXPR"],
-        vstring: ["TERMO", "EXPR"],
+    65: {
+        3: "p34",
+        4: "p34",
+        20: "p34",
+        "24": "p34",
+        "25": "p34",
+        "26": "p34",
+        "27": "p34",
+        "28": "p34",
+        "29": "p34",
+        "30": "p34",
+        31: "p34",
+        34: "p33",
+        36: "p34",
+        37: "p32",
+        39: "p42",
+        41: "p34"   
     },
-
-    EXPR: {
-        "+": ["+", "TERMO", "EXPR"],
-        "-": ["-", "TERMO", "EXPR"],
-        ";": ["ε"],
-        ")": ["ε"],
-        ",": ["ε"],
-        then: ["ε"],
-        do: ["ε"],
-        "=": ["ε"],
-        "<>": ["ε"],
-        "<": ["ε"],
-        ">": ["ε"],
-        "<=": ["ε"],
-        ">=": ["ε"],
-        '}': ['ε'],
-        'to': ['ε']
+    66: {
+        11: "p42",
+        12: "p42",
+        13: "p42",
+        16: "p42",
+        23: "p42",
     },
-
-    TER: {
-        "*": ["*", "FATOR", "TER"],
-        "/": ["/", "FATOR", "TER"],
-        "+": ["ε"],
-        "-": ["ε"],
-        ";": ["ε"],
-        ")": ["ε"],
-        ",": ["ε"],
-        then: ["ε"],
-        do: ["ε"],
-        "=": ["ε"],
-        "<>": ["ε"],
-        "<": ["ε"],
-        ">": ["ε"],
-        "<=": ["ε"],
-        ">=": ["ε"],
-        '}': ['ε'],
-        'to': ['ε']
+    67: {
+        19: "p49",
+        31: "p50",
     },
-
-    TERMO: {
-        "(": ["FATOR", "TER"],
-        ident: ["FATOR", "TER"],
-        nint: ["FATOR", "TER"],
-        nreal: ["FATOR", "TER"],
-        literal: ["FATOR", "TER"],
-        vstring: ["FATOR", "TER"],
+    68: {
+        "24": "p48",
+        "25": "p46",
+        "26": "p43",
+        "27": "p44",
+        "28": "p47",
+        "29": "p45",
+        39: "p53",
     },
-
-    FATOR: {
-        "(": ["(", "EXPRESSAO", ")"],
-        ident: ["ident"],
-        nint: ["nint"],
-        nreal: ["nreal"],
-        literal: ["literal"],
-        vstring: ["vstring"],
+    69: {
+        "32": "p54",
+        39: "p55",
     },
-
-    EXPRELACIONAL: {
-        "(": ["EXPRESSAO", "OPREL", "EXPRESSAO"],
-        ident: ["EXPRESSAO", "OPREL", "EXPRESSAO"],
-        nint: ["EXPRESSAO", "OPREL", "EXPRESSAO"],
-        nreal: ["EXPRESSAO", "OPREL", "EXPRESSAO"],
-        literal: ["EXPRESSAO", "OPREL", "EXPRESSAO"],
-        vstring: ["EXPRESSAO", "OPREL", "EXPRESSAO"],
+    70: {
+        31: "p56",
     },
-
-    OPREL: {
-        "=": ["="],
-        "<>": ["<>"],
-        "<": ["<"],
-        ">": [">"],
-        "<=": ["<="],
-        ">=": [">="],
+    71: {
+        11: "p59",
+        12: "p58",
+        13: "p65",
+        16: "p57",
+        23: "p60",
+    },
+    72: {
+        36: "p61",
+        38: "p62",
     },
 };
 
-const tokensMapInverse = {
-    1: "while",
-    2: "var",
-    3: "to",
-    4: "then",
-    5: "string",
-    6: "real",
-    7: "read",
-    8: "program",
-    9: "procedure",
-    10: "print",
-    11: "nreal",
-    12: "nint",
-    13: "literal",
-    14: "integer",
-    15: "if",
-    16: "ident",
-    17: "for",
-    18: "end",
-    19: "else",
-    20: "do",
-    21: "const",
-    22: "begin",
-    23: "vstring",
-    24: ">=",
-    25: ">",
-    26: "=",
-    27: "<>",
-    28: "<=",
-    29: "<",
-    30: "+",
-    31: ";",
-    32: ":=",
-    33: ":",
-    34: "/",
-    35: ".",
-    36: ",",
-    37: "*",
-    38: ")",
-    39: "(",
-    40: "{",
-    41: "}",
-    42: "-",
-    43: "$",
-};
 
-const simboloInicial = "PROGRAMA";
-const terminalEOF = "$";
-const producaoVazia = "ε";
+exports.parser = (tokens) => {
+    const pilha = [43, ...this.producoes["p1"].reverse()];
+    const entrada = [...tokens,{
+        lexema: "$",
+        token: 43,
+        line: -1
+    }];
 
-// Ex: tokens gerados pelo analisador léxico
-// const tokens = [
-//   { lexema: 'program', token: 8, line: 1 },
-//   { lexema: 'x', token: 16, line: 1 },
-//   { lexema: ';', token: 31, line: 1 },
-//   ...
-// ];
-
-exports.parser = (tokens, tabela) => {
-    const pilha = [terminalEOF, simboloInicial];
-    const entrada = [...tokens, { token: 43, lexema: "$", line: -1 }]; // adiciona EOF
-    let index = 0;
-
+    const derivacoes = [];
+    const erros = [];
+    let index = 1;
     while (pilha.length > 0) {
         const topo = pilha[pilha.length - 1];
-        const atual = entrada[index];
-        const terminalAtual = tokensMapInverse[atual.token];
-
-        console.log(`\nPilha: [${pilha.join(", ")}]`);
-        console.log(
-            `Token atual: '${atual.lexema}' (cód ${atual.token}) na linha ${atual.line}`
-        );
-
-        // Terminal
-        if (!tabela[topo]) {
-            if (topo === terminalAtual) {
-                pilha.pop();
-                index++;
-            } else {
-                console.error(
-                    `Erro sintático na linha ${atual.line}: esperado '${topo}', encontrado '${terminalAtual}'`
-                );
-                return;
-            }
+        const tokenAtual = entrada[0];
+        console.log(pilha)
+        console.log(topo)
+        console.log("tokenAtual", tokenAtual)
+        if (!tokenAtual) {
+            erros.push(`Erro: token inesperado no final da entrada.`);
+            break;
         }
-        // Não-terminal
-        else {
-            const producoes = tabela[topo];
-            const producao = producoes[terminalAtual];
 
-            if (!producao) {
-                console.error(
-                    `Erro sintático na linha ${atual.line}: símbolo inesperado '${terminalAtual}' para '${topo}'`
-                );
-                return;
-            }
-
+        if (topo === 43 && tokenAtual.token === -1) {
             pilha.pop();
-            if (producao[0] !== producaoVazia) {
+            entrada.shift();
+        } else if (!isNaN(Number(topo)) && Number(topo) === tokenAtual.token) {
+            // Match terminal
+            pilha.pop();
+            entrada.shift();
+        } else if (
+            this.matrizParsing[topo] &&
+            this.matrizParsing[topo][tokenAtual.token] !== undefined
+        ) {
+            // Não-terminal com entrada válida
+            const prodIndex = this.matrizParsing[topo][tokenAtual.token];
+            const producao = this.producoes[prodIndex];
+
+            pilha.pop(); // remove o não-terminal
+
+            if (producao[0] !== 44) {
                 for (let i = producao.length - 1; i >= 0; i--) {
                     pilha.push(producao[i]);
                 }
             }
+
+            derivacoes.push(`${topo} → ${producao.join(" ")}`);
+        } else if(topo == 44){
+            pilha.pop()
+        } else if(this.producoes[topo]){
+            pilha.pop(); // remove o não-terminal
+            const producao = this.producoes[topo]
+            for (let i = producao.length - 1; i >= 0; i--) {
+                pilha.push(producao[i]);
+            }
+            
+
+            derivacoes.push(`${topo} → ${producao.join(" ")}`);
+        } else {
+            erros.push(
+                `Erro sintático na linha ${tokenAtual.line}: inesperado '${tokenAtual.lexema}'`
+            );
+            break;
         }
+        index++;
     }
 
-    if (index === entrada.length) {
+    if (erros.length == 0) {
         console.log("\n✅ Análise sintática concluída com sucesso!");
     } else {
+        console.log(erros)
         console.error("\n❌ Fim da pilha alcançado antes do fim da entrada.");
     }
+
+    return { derivacoes, erros };
 };
