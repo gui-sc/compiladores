9dd1c1cd90c5f709a58274d2349dcd8cb1f7fa50
guilherme coelho <guilhermeifc7@gmail.com>
2025-05-30 19:15:01 -0300

    fix: corrige lógica de laços e ajusta a leitura de arquivos no analisador

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const {processLexico} = require('./lexico')
 const {parser, tabela} = require('./sintatico')
+const fs = require('fs')
 
 function execute() {
     // const arquivo = process.argv[2];
-    const arquivo = "./exemplos/procedures.txt"
+    const arquivo = "./exemplos/lacos.txt"
     const {erros, tokens} = processLexico(arquivo)
+    fs.writeFileSync('tokens.json', JSON.stringify(tokens))
     if(erros.length > 0){
         console.log("Erros léxicos encontrados: ", erros.join("\n"))
         return;
